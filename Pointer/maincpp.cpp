648235b18b80b3dd9c0f7b3881ee5806fe8cdb1c
- <-@DESKTOP-5LUH4K3>
2023-07-11 16:14:05 +0300

    Add project files.

---
+++ Pointer/maincpp.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+//#define POINTERS_BASICS
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef POINTERS_BASICS
+
+
+	int a = 2;
+	int* pa = &a;
+	// pa - pointer to 'a'
+	std::cout << a << std::endl; // вывод значения переменной a на экран
+	std::cout << &a << std::endl; // взятие адреса переменной a прямо при выводе
+
+	std::cout << pa << std::endl; // вывод адреса переменной a хранящегося в указатели pa
+	std::cout << *pa << std::endl; // разименновывыем укзатель pa и получаем значение переменной a
+
+	int* pb;
+	int b = 3;
+	pb = &b;
+
+	std::cout << *pb << std::endl;
+
+	//int - int
+	//int* - указатель на int
+	// double - double
+	// double* - указатель на double 
+
+#endif // POINTERS_BASICS
+
+
+	const int n = 5;
+	short arr[n] = { 3,5,8,13,21 };
+
+	std::cout << arr << std::endl;
+	std::cout << *arr << std::endl;
+	
+
+	for (int  i = 0; i < n; i++)
+	{
+		std::cout << *(arr+i) << "\t";
+		
+	}
+	std::cout << std::endl;
+}
---
+++ TransferParameters/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void Exchange(int a, int b);
+
+void main() 
+{
+	setlocale(LC_ALL, "");
+
+	int a = 2;
+	int b = 3;
+
+	std::cout << a << "\t" << b << std::endl;
+	Exchange(a, b);
+	std::cout << a << "\t" << b << std::endl;
+
+}
+
+void Exchange(int a, int b) 
+
+{
+	int buffer = a;
+	a = b;
+	b = buffer;
+	
+}
