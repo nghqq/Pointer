c28fe3d399cafe7d2e42ef011389d0cb4090b817
- <-@DESKTOP-5LUH4K3>
2023-08-21 13:52:09 +0300

    DONE

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -6,6 +6,10 @@
 #define DYNAMIC_MEMORY_1
 #define DYNAMIC_MEMORY_2
 
+
+int** Allocate(int** arr, const int ROWS, const int COLS);
+void Clear(int** arr, const int ROWS);
+
 void FillRand(int arr[], const int n);
 void FillRand(int** arr, const int COLS, const int ROWS);
 
@@ -32,7 +36,14 @@ int** pop_row_back(int** arr, int& ROWS, const int COLS);
 int** erase_row(int** arr, int& ROWS, const int COLS, int index);
 
 int** push_col_back(int** arr, int& ROWS, int& COLS);
-//int** push_col_front(int** arr, int& ROWS, int& COLS);
+int** push_col_front(int** arr, int& ROWS, int& COLS);
+int** insert_col(int** arr, int& ROWS, int& COLS, int index);
+
+
+void pop_col_back(int** arr, int& ROWS, int& COLS);
+void  pop_col_front(int** arr, int& ROWS, int& COLS);
+void erase_col(int** arr, int& ROWS, int& COLS, int index);
+
 
 
 void main()
@@ -74,55 +85,119 @@ void main()
 
 	std::cout << "Введите кол-во строк: "; std::cin >> ROWS;
 	std::cout << "Введите кол-во элементов в строке: "; std::cin >> COLS;
-
 	int** arr = new int* [ROWS];
-	for (int i = 0; i < ROWS; i++)
-	{
-		arr[i] = new int[COLS] {};
-	}
+	arr = Allocate(arr,ROWS,COLS);
+	std::cout << "Двумерный массив случайных чисел: ";
+	std::cout << std::endl;
 	FillRand(arr, ROWS, COLS);
 	Print(arr, ROWS, COLS);
 	std::cout << delimeter;
+	std::cout << "Добавляем строку в конец: ";
+	std::cout << std::endl;
 	arr = push_row_back(arr, ROWS, COLS);
 	Print(arr, ROWS, COLS);
 	std::cout << delimeter;
+	std::cout << "Добавляем строку в начало: ";
+	std::cout << std::endl;
 	arr = push_row_front(arr, ROWS, COLS);
 	Print(arr, ROWS, COLS);
 	std::cout << delimeter;
 	std::cout << std::endl;
 	int index;
+	std::cout << "Добавляем строку по индексу: ";
+	std::cout << std::endl;
 	std::cout << "Введите индекс: "; std::cin >> index;
 	std::cout << std::endl;
 	arr = insert_row(arr, ROWS, COLS, index);
 	Print(arr, ROWS, COLS);
 	std::cout << delimeter;
+	std::cout << "Удаляем первую строчку: ";
+	std::cout << std::endl;
 	arr = pop_row_front(arr, ROWS, COLS);
 	Print(arr, ROWS, COLS);
 	std::cout << delimeter;
+	std::cout << "Удаляем последнюю строчку: ";
+	std::cout << std::endl;
 	arr = pop_row_back(arr, ROWS, COLS);
 	Print(arr, ROWS, COLS);
 	std::cout << delimeter;
+	std::cout << "Удаляем строчку по индексу: ";
+	std::cout << std::endl;
 	std::cout << "Введите индекс: "; std::cin >> index;
 	std::cout << std::endl;
 	arr = erase_row(arr, ROWS, COLS, index);
 	Print(arr, ROWS, COLS);
 	std::cout << delimeter;
 
+	std::cout << "Добавляем столбец в конец: ";
+	std::cout << std::endl;
 	arr = push_col_back(arr, ROWS, COLS);
 	Print(arr, ROWS, COLS);
 	std::cout << delimeter;
 	std::cout << std::endl;
 
-	
+	std::cout << "Добавляем столбец в начало: ";
+	std::cout << std::endl;
+	arr = push_col_front(arr, ROWS, COLS);
+	Print(arr, ROWS, COLS);
+	std::cout << delimeter;
+	std::cout << std::endl;
 
-	
+
+	std::cout << "Добавляем столбец по индексу: ";
+	std::cout << std::endl;
+	std::cout << "Введите индекс: "; std::cin >> index;
+	std::cout << std::endl;
+	arr = insert_col(arr, ROWS, COLS,index);
+	Print(arr, ROWS, COLS);
+	std::cout << delimeter;
+	std::cout << std::endl;
+
+
+	std::cout << "Удаляем последний столбец: ";
+	std::cout << std::endl;
+	pop_col_back(arr, ROWS, COLS);
+	Print(arr, ROWS, COLS);
+	std::cout << delimeter;
+	std::cout << std::endl;
+
+	std::cout << "Удаляем первый столбец: ";
+	std::cout << std::endl;
+	pop_col_front(arr, ROWS, COLS);
+	Print(arr, ROWS, COLS);
+	std::cout << delimeter;
+	std::cout << std::endl;
+
+
+	std::cout << "Удаляем столбец по индексу: ";
+	std::cout << std::endl;
+	std::cout << "Введите индекс: "; std::cin >> index;
+	std::cout << std::endl;
+	erase_col(arr, ROWS, COLS, index);
+	Print(arr, ROWS, COLS);
+	std::cout << delimeter;
+	std::cout << std::endl;
+
+	Clear(arr, ROWS);
+
+}
+
+int** Allocate(int** arr, const int ROWS, const int COLS) 
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		arr[i] = new int[COLS] {};
+	}
+	return arr;
+}
+
+void Clear(int** arr, const int ROWS) 
+{
 	for (int i = 0; i < ROWS; i++)
 	{
 		delete arr[i];
 	}
 	delete[] arr;
-
-
 }
 
 void FillRand(int arr[], const int n)
@@ -334,23 +409,110 @@ int** erase_row(int** arr, int& ROWS, const int COLS, int index)
 
 int** push_col_back(int** arr, int& ROWS, int& COLS)
 {
-	int** buffer = new int* [COLS + 1] {};
 	for (int i = 0; i < ROWS; i++)
 	{
-		buffer[i] = arr[i];
-
+		int* buffer = new int[COLS + 1] {};
+		for (int j = 0; j < COLS; j++)
+		{
+			buffer[j] = arr[i][j];
+		}
+		delete[] arr[i];
+		arr[i] = buffer;
 	}
-	for (int i = 0; i <= COLS; i++)
-	{
-		arr[i][COLS] = 0;
-	}
-	
-	
-	
-	delete[]arr;
 	COLS++;
-	return buffer;
+	return arr;
 }
+
+int** push_col_front(int** arr, int& ROWS, int& COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		int* buffer = new int[COLS + 1] {};
+		for (int j = 0; j < COLS; j++)
+		{
+			buffer[j + 1] = arr[i][j];
+		}
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	COLS++;
+	return arr;
+}
+
+int** insert_col(int** arr, int& ROWS, int& COLS, int index) 
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		int* buffer = new int[COLS + 1] {};
+		for (int j = 0; j < COLS; j++)
+		{
+			if (j < index)
+				buffer[j] = arr[i][j];
+			else
+				buffer[j + 1] = arr[i][j];
+		}
+	delete[] arr[i];
+	arr[i] = buffer;
+	}
+	COLS++;
+	return arr;
+}
+
+void  pop_col_back(int** arr,  int& ROWS, int& COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		int* buffer = new int[COLS] {};
+		for (int j = 0; j < COLS; j++)
+		{
+			buffer[j] = arr[i][j];
+		}
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	COLS--;
+	
+}
+
+void  pop_col_front(int** arr, int& ROWS, int& COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		int* buffer = new int[COLS] {};
+		for (int j = 0; j < COLS; j++)
+		{
+			buffer[j] = arr[i][j+1];
+		}
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	COLS--;
+
+}
+
+void erase_col(int** arr, int& ROWS, int& COLS, int index)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		int* buffer = new int[COLS] {};
+		for (int j = 0; j < COLS; j++)
+		{
+			if (j < index)
+				buffer[j] = arr[i][j];
+			else
+				buffer[j] = arr[i][j+1];
+		}
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	COLS--;
+	
+}
+
+
+
+
+
 
 
 
